8c5515b651135045a1b12dad7a016a4820556493
Igor <igormarianfr@gmail.com>
2022-04-14 18:18:45 +0300

    Add the second task

--- src/main/java/ru/gb/marian/Homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/marian/Homework1/HomeWorkApp.java
@@ -1,7 +1,17 @@
 package ru.gb.marian.Homework1;
 
 public class HomeWorkApp {
+    public static void printThreeWords() {
+        System.out.println
+                ("Orange\n"+
+            "Banana\n"+
+        "Apple\n");
+
+    }
+
     public static void main(String[] args) {
+
+        printThreeWords();
 
     }
 }
