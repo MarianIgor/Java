5c04a1b7cf95fa986f6c977c189c69e9b1d701f6
Igor <igormarianfr@gmail.com>
2022-04-14 16:45:55 +0300

    Initial

--- src/main/java/ru/gb/marian/Main.java
+++ src/main/java/ru/gb/marian/Main.java
@@ -3,7 +3,7 @@ package ru.gb.marian;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Привет");
+        System.out.println("Привет!");
     }
 
 }
